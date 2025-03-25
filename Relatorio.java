e3a097209189e2375f994f5b295d837bc8278f9e
Arthur1701 <arthur17agb@gmail.com>
2025-03-25 19:34:28 -0300

    Create Relatorio.java

---
+++ Relatorio.java
@@ -0,0 +1,14 @@
+class Relatorio {
+    private int id;
+    private Date dataGeracao;
+    private float consumoTotal;
+
+    public int getId() { return id; }
+    public void setId(int id) { this.id = id; }
+
+    public Date getDataGeracao() { return dataGeracao; }
+    public void setDataGeracao(Date dataGeracao) { this.dataGeracao = dataGeracao; }
+
+    public float getConsumoTotal() { return consumoTotal; }
+    public void setConsumoTotal(float consumoTotal) { this.consumoTotal = consumoTotal; }
+}
